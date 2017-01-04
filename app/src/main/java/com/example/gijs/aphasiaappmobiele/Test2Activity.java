28b92a6a6af8bf3c2ad53e63e418e538e89b0cb1
gijsVerheyden <https://gijsver@bitbucket.org/gijsver/picross.git>
2017-01-04 23:26:53 +0100

    First Commit
    First Commit

--- app/src/main/java/com/example/gijs/aphasiaappmobiele/Test2Activity.java
+++ app/src/main/java/com/example/gijs/aphasiaappmobiele/Test2Activity.java
@@ -2,17 +2,20 @@ package com.example.gijs.aphasiaappmobiele;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
 
-/**
- * Created by gijs on 29/12/2016.
- */
+import java.util.List;
 
 public class Test2Activity extends AppCompatActivity {
+    private ListView pictures;
+    private List<Integer> picturesID;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.test2);
-
+        pictures = (ListView) findViewById(R.id.image_list);
+        pictures.setAdapter(new PictureListAdapter(Test2Activity.this,picturesID));
     }
 }
--- app/src/main/java/com/example/gijs/aphasiaappmobiele/Test3Activity.java
+++ app/src/main/java/com/example/gijs/aphasiaappmobiele/Test3Activity.java
@@ -2,17 +2,21 @@ package com.example.gijs.aphasiaappmobiele;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
 
-/**
- * Created by gijs on 29/12/2016.
- */
+import java.util.List;
+
 
 public class Test3Activity extends AppCompatActivity {
+    private ListView pictures;
+    private List<Integer> picturesID;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.test3);
-
+        pictures = (ListView) findViewById(R.id.image_list);
+        pictures.setAdapter(new PictureListAdapter(Test3Activity.this,picturesID));
     }
 }
